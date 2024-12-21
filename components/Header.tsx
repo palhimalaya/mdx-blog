3df6daaf4c689efe73b376fdbcefae0cc1102aad
palhimalaya <himalayapal123@gmail.com>
2024-12-21 11:17:33 +0545

    adjust layout of header component for improved spacing in navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ export default async function Header() {
     <header className='fixed inset-x-0 top-0 z-50 bg-background/75 py-6 backdrop-blur-sm'>
       <nav className='container flex max-w-3xl items-center justify-between'>
         <NavMenu isAdmin={isAdmin} />
-        <div className='flex items-center justify-center gap-5'>
+        <div className='flex items-center justify-center gap-5 ml-10'>
           <ThemeToggle />
           <SignedIn>
             <UserButton />
